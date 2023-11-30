90bdedd545825f11f91e3906ce951a5e26dfd500
David de Meijere <daviddm@daviddm.lan>
2023-11-30 23:31:13 +0100

    first

---
+++ template/index.test.ts
@@ -0,0 +1,37 @@
+import { readFile } from "@util/readFile";
+import { stringToNumber } from "@util/stringToNumber";
+import { run } from "./";
+
+describe("today", () => {
+  let list: string[];
+  let testList: string[];
+
+  beforeEach(async () => {
+    list = await readFile(`${__dirname}/input.txt`);
+    testList = await readFile(`${__dirname}/test.txt`);
+  });
+
+  describe("test", () => {
+    it("run", async () => {
+      expect.assertions(1);
+      const result = await run(testList);
+      expect(result).toEqual(undefined);
+    });
+  });
+
+  describe.skip("first", () => {
+    it("run", async () => {
+      expect.assertions(1);
+      const result = await run(list);
+      expect(result).toEqual(undefined);
+    });
+  });
+
+  describe.skip("second", () => {
+    it("run", async () => {
+      expect.assertions(1);
+      const result = await run(list);
+      expect(result).toEqual(undefined);
+    });
+  });
+});
